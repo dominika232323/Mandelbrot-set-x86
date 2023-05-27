87ba4a05e8643cd8ccf1e09a752875c770bc31ce
dominika232323 <dominikaboguszewska62@gmail.com>
2023-05-27 18:24:45 +0200

    mandlebrot.asm makes whole picture white

--- mandelbrot.asm
+++ mandelbrot.asm
@@ -10,21 +10,63 @@ mandelbrot:
     ; rdx - imageHeight
 
 padding:
-    mov rax, rsi
-    imul rax, rdi
-    imul rax, 3
-    mov r10, rax        ; r10 - imageSize
-
     mov r8, 0b0011
-    mov rax, r10
-    and rax, r8
+    mov rax, rsi
+    and rax, r8         ; rax = width % 4
 
     mov r9, 4
     sub rax, r9
-    imul rax, -1
 
     and rax, r8
     mov r9, rax         ; r9 - padding
+
+before_height_loop:
+    mov r8, rdx
+    dec r8              ; r8 - height iterator
+
+height_loop:
+    mov r11, 0          ; r11 - width iterator
+
+    cmp r8, 0
+    jl end_loop
+
+width_loop:
+    cmp r11, rsi
+    jge next_height
+
+    ; complex nums
+
+store:
+    ; store blue
+    mov byte [rdi], 255
+
+    ; store green
+    inc rdi
+    mov byte [rdi], 255
+
+    ; store red
+    inc rdi
+    mov byte [rdi], 255
+
+    inc rdi
+
+    inc r11
+    jmp width_loop
+
+next_height:
+    dec r8
+
+    mov r12, r9
+
+skip_padding:
+    test r12, r12
+    jz height_loop
+
+    inc rdi
+    dec r12
+    jmp skip_padding
+
+end_loop:
 
 end:
 
