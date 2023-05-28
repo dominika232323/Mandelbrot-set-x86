ea9ff5bff5606fa70f58d47ee2306fcad014d39c
dominika232323 <dominikaboguszewska62@gmail.com>
2023-05-28 23:01:57 +0200

    Come back to working version

--- main.cpp
+++ main.cpp
@@ -66,55 +66,18 @@ void loadBMP(const char* filename) {
 
 
 void renderScene() {
-    printf("koooooooooooooooko");
     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-
-    // Apply zoom
-
-
-    // Calculate translated center position
-    float centerX = static_cast<float>(imageWidth) / 2.0f;
-    float centerY = static_cast<float>(imageHeight) / 2.0f;
-
-    // Translate to the center
-    glTranslatef(-centerX, -centerY, 0.0f);
-
-    // Draw the image
     glDrawPixels(imageWidth, imageHeight, GL_RGB, GL_UNSIGNED_BYTE, imageData);
-    glScalef(zoomFactor, zoomFactor, 1.0f);
     glFlush();
-}
-
-
-void keyboardFunc(unsigned char key, int x, int y) {
-    switch (key) {
-        case '+': // Zoom-in
-            zoomFactor *= 1.1f;
-            printf("%f\n", zoomFactor);
-            break;
-        case '-': // Zoom-out
-            zoomFactor /= 1.1f;
-            break;
-    }
-    glutPostRedisplay();
-}
-
-
-void displayFunc() {
-    printf("aasaaaa");
-    renderScene();
-    glutSwapBuffers();
 }
 
 
 void initializeOpenGL(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
     glutInitWindowSize(imageWidth, imageHeight);
     glutCreateWindow("Mandelbrot set");
-    glutKeyboardFunc(keyboardFunc);
-    glutDisplayFunc(displayFunc);
+    glutDisplayFunc(renderScene);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     gluOrtho2D(0, imageWidth, 0, imageHeight);
     glutMainLoop();
@@ -127,13 +90,14 @@ int main(int argc, char** argv) {
 
     // /mnt/c/Users/domin/Desktop/studia/sem2_23L/ARKO/x86/Mandelbrot-set-x86/lena.bmp
 
-    // printf("Welcome to mandelbrot set generator!\n");
-    // printf("Give the absolute path to the bmp file that you want to change to mandelbrot set: ");
-    // scanf("%s", filename);
+    printf("Welcome to mandelbrot set generator!\n");
+    printf("Give the absolute path to the bmp file that you want to change to mandelbrot set: ");
+    scanf("%s", filename);
 
     loadBMP(filename);
     initializeOpenGL(argc, argv);
 
     delete[] imageData;
+
     return 0;
 }
